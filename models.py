ac4ed47a25c4cf2c46ce0f90d8f00d578e8084f1
Nynia <jokeforce@gmail.com>
2017-05-31 12:58:17 +0800

    debug

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql://root:admin@127.0.0.1/music_spider?charset=utf8', echo=False)
+engine = create_engine('mysql://root:admin@192.168.127.55/music_spider?charset=utf8', echo=False)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
