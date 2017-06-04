2b748698090dcf1abaf8e84ee2edf4d36642c1ff
Nynia <jokeforce@gmail.com>
2017-06-04 23:01:16 +0800

    debug

--- spider.py
+++ spider.py
@@ -116,7 +116,7 @@ def get_artist_by_category_id(artist_category_id_list):
     return artist_id_list
 
 
-def get_album_by_artist(artist_list):
+def get_album_by_artist(artist_list,fp,lock):
     #print '%s start' % threading.current_thread().getName()
     proxies = None
     album_list = []
@@ -173,10 +173,13 @@ def get_album_by_artist(artist_list):
                             #             break
                             # json_result = json.loads(r.content)
                             # song_list.append(json_result)
-    print len(song_id)
-    fpout = open('song_id_result','w')
-    fpout.write(','.join(song_id))
-    fpout.close()
+
+    while True:
+        print '%s lock acquiring...' % threading.current_thread().getName()
+        lock.acquire()
+        fp.write('\n'.join(song_list))
+        lock.release()
+
 
 def get_song_by_album(album_list):
     song_list = []
@@ -220,174 +223,176 @@ def get_song_by_album(album_list):
     t.join()
     print 'thread analyse_song end'
 
-def analyse_song_page(lock,song_queue):
+def analyse_song_page(song_list):
     print '%s start' % threading.current_thread().getName()
     proxies = None
-    exitFlag = False
-    song_list = []
-    while True:
-        print '%s lock acquiring...' % threading.current_thread().getName()
-        lock.acquire()
-        if not song_queue.empty():
-            song_list = song_queue.get()
-        lock.release()
-        db_song = []
-        db_album = []
-        db_comment = []
-        for song in song_list:
-            song_json = song['songs'][0]
-            album_json = song_json['album']
-            artist_json = song_json['artists'][0]
-
-            song_id = song_json['id']
-            song_name = song_json['name']
-            song_duration = song_json['duration']
-            artist_id = artist_json['id']
-            album_id = album_json['id']
-            album_name = album_json['name']
-            album_size = album_json['size']
-            album_cover = album_json['picUrl']
-            release_time = album_json['publishTime']
-            release_comp = album_json['company']
-
-            comment_thread = song_json['commentThreadId']
-            print '%s song_id:%s' % (threading.current_thread().getName(),song_id)
-
-            url = 'http://music.163.com/weapi/v1/resource/comments/' + comment_thread
-            params = get_params(1)
-            encSecKey = get_encSecKey()
-            data = {
-                "params": params,
-                "encSecKey": encSecKey
-            }
-            while True:
-                try:
-                    r = requests.post(url, data=data, proxies=proxies)
-                except requests.exceptions.RequestException:
+    db_song = []
+    db_album = []
+    db_comment = []
+    for song_id in song_list:
+        url = 'http://music.163.com/api/song/detail/?id=%s&ids=[%s]' % (song_id, song_id)
+        while True:
+            try:
+                r = requests.get(url, proxies=proxies)
+            except requests.exceptions.RequestException:
+                proxies = get_availalbe_proxy()
+            else:
+                if r.status_code != 200:
                     proxies = get_availalbe_proxy()
                 else:
-                    if r.status_code != 200:
-                        proxies = get_availalbe_proxy()
-                    else:
-                        break
+                    break
+        song = json.loads(r.content)
 
-            json_dict = json.loads(r.content)
-            total = json_dict['total']
+        song_json = song['songs'][0]
+        album_json = song_json['album']
+        artist_json = song_json['artists'][0]
 
-            song = Song()
-            song.id = song_id
-            song.name = song_name
-            song.duration = song_duration
-            song.album_id = album_id
-            song.artist_id = artist_id
-            song.comment_count = total
-            db_song.append(song)
+        song_id = song_json['id']
+        song_name = song_json['name']
+        song_duration = song_json['duration']
+        artist_id = artist_json['id']
+        album_id = album_json['id']
+        album_name = album_json['name']
+        album_size = album_json['size']
+        album_cover = album_json['picUrl']
+        release_time = album_json['publishTime']
+        release_comp = album_json['company']
 
-            album = Album()
-            album.id = album_id
-            album.alb_name = album_name
-            album.alb_size = album_size
-            album.alb_cover = album_cover
-            album.artist_id = artist_id
+        comment_thread = song_json['commentThreadId']
+        print '%s song_id:%s' % (threading.current_thread().getName(),song_id)
+
+        url = 'http://music.163.com/weapi/v1/resource/comments/' + comment_thread
+        params = get_params(1)
+        encSecKey = get_encSecKey()
+        data = {
+            "params": params,
+            "encSecKey": encSecKey
+        }
+        while True:
             try:
-                timestamp = str(release_time)
-                album.release_time = time.strftime('%Y-%m-%d',
-                                                   time.localtime(float(timestamp[:10] + '.' + timestamp[-3:])))
-            except:
-                print 'timestamp error:%s' % release_time
-                album.release_time = ''
-            album.release_comp = release_comp
-            if album.id not in [item.id for item in db_album]:
-                db_album.append(album)
-
-            hot_comments = json_dict['hotComments']
-            for item in hot_comments:
-                comment = Comment()
-                comment.song_id = int(song_id)
-                # 过滤emoji表情
-                try:
-                    highpoints = re.compile(u'[\U00010000-\U0010ffff]')
-                except re.error:
-                    highpoints = re.compile(u'[\uD800-\uDBFF][\uDC00-\uDFFF]')
-
-                content = highpoints.sub(u'??', item['content'])
-                comment.content = content[:300]
-                comment.liked_count = int(item['likedCount'])
-                timestamp = str(item['time'])
-                comment.timestamp = time.strftime('%Y%m%d%H%M%S',
-                                                  time.localtime(float(timestamp[:10] + '.' + timestamp[-3:])))
-                comment.user_id = int(item['user']['userId'])
-                comment.nickname = item['user']['nickname']
-                db_comment.append(comment)
-
-            #写入数据库
-            while True:
-                try:
-                    session = Session()
-                    session.execute(
-                        Song.__table__.insert().prefix_with('IGNORE'),
-                        [{'id': song.id,
-                          'name': song.name,
-                          'duration': song.duration,
-                          'artist_id': song.artist_id,
-                          'album_id': song.album_id,
-                          'artist_id': song.artist_id,
-                          'comment_count': song.comment_count
-                          } for song in db_song]
-                    )
-                except exc.OperationalError:
-                    print 'OperationalError'
-                    session.rollback()
-                    continue
-                finally:
-                    session.commit()
-                    Session.remove()
-                    break
-            while True:
-                try:
-                    session = Session()
-                    session.execute(
-                        Comment.__table__.insert().prefix_with('IGNORE'),
-                        [{'song_id': comment.song_id,
-                          'content': comment.content,
-                          'liked_count': comment.liked_count,
-                          'timestamp': comment.timestamp,
-                          'user_id': comment.user_id,
-                          'nickname': comment.nickname
-                          } for comment in db_comment]
-                    )
-                except exc.OperationalError:
-                    print 'OperationalError'
-                    session.rollback()
-                    continue
-                finally:
-                    session.commit()
-                    Session.remove()
-                    break
-            while True:
-                try:
-                    session = Session()
-                    for album in db_album:
-                        session.execute(
-                            Album.__table__.insert().prefix_with('IGNORE'),
-                            {'id': album.id,
-                             'alb_name': album.alb_name,
-                             'alb_desc': '',
-                             'alb_cover': album.alb_cover,
-                             'alb_size': album.alb_size,
-                             'artist_id': album.artist_id,
-                             'release_time': album.release_time,
-                             'release_comp': album.release_comp}
-                        )
-                except exc.OperationalError:
-                    print 'OperationalError'
-                    session.rollback()
-                    continue
-                finally:
-                    session.commit()
-                    Session.remove()
+                r = requests.post(url, data=data, proxies=proxies)
+            except requests.exceptions.RequestException:
+                proxies = get_availalbe_proxy()
+            else:
+                if r.status_code != 200:
+                    proxies = get_availalbe_proxy()
+                else:
                     break
 
+        json_dict = json.loads(r.content)
+        total = json_dict['total']
+
+        song = Song()
+        song.id = song_id
+        song.name = song_name
+        song.duration = song_duration
+        song.album_id = album_id
+        song.artist_id = artist_id
+        song.comment_count = total
+        db_song.append(song)
+
+        album = Album()
+        album.id = album_id
+        album.alb_name = album_name
+        album.alb_size = album_size
+        album.alb_cover = album_cover
+        album.artist_id = artist_id
+        try:
+            timestamp = str(release_time)
+            album.release_time = time.strftime('%Y-%m-%d',
+                                               time.localtime(float(timestamp[:10] + '.' + timestamp[-3:])))
+        except:
+            print 'timestamp error:%s' % release_time
+            album.release_time = ''
+        album.release_comp = release_comp
+        if album.id not in [item.id for item in db_album]:
+            db_album.append(album)
+
+        hot_comments = json_dict['hotComments']
+        for item in hot_comments:
+            comment = Comment()
+            comment.song_id = int(song_id)
+            # 过滤emoji表情
+            try:
+                highpoints = re.compile(u'[\U00010000-\U0010ffff]')
+            except re.error:
+                highpoints = re.compile(u'[\uD800-\uDBFF][\uDC00-\uDFFF]')
+
+            content = highpoints.sub(u'??', item['content'])
+            comment.content = content[:300]
+            comment.liked_count = int(item['likedCount'])
+            timestamp = str(item['time'])
+            comment.timestamp = time.strftime('%Y%m%d%H%M%S',
+                                              time.localtime(float(timestamp[:10] + '.' + timestamp[-3:])))
+            comment.user_id = int(item['user']['userId'])
+            comment.nickname = item['user']['nickname']
+            db_comment.append(comment)
+    while True:
+        try:
+            session = Session()
+            session.execute(
+                Song.__table__.insert().prefix_with('IGNORE'),
+                [{'id': song.id,
+                  'name': song.name,
+                  'duration': song.duration,
+                  'artist_id': song.artist_id,
+                  'album_id': song.album_id,
+                  'artist_id': song.artist_id,
+                  'comment_count': song.comment_count
+                  } for song in db_song]
+            )
+        except exc.OperationalError:
+            print 'OperationalError'
+            session.rollback()
+            continue
+        finally:
+            session.commit()
+            Session.remove()
+            break
+    while True:
+        try:
+            session = Session()
+            session.execute(
+                Comment.__table__.insert().prefix_with('IGNORE'),
+                [{'song_id': comment.song_id,
+                  'content': comment.content,
+                  'liked_count': comment.liked_count,
+                  'timestamp': comment.timestamp,
+                  'user_id': comment.user_id,
+                  'nickname': comment.nickname
+                  } for comment in db_comment]
+            )
+        except exc.OperationalError:
+            print 'OperationalError'
+            session.rollback()
+            continue
+        finally:
+            session.commit()
+            Session.remove()
+            break
+    while True:
+        try:
+            session = Session()
+            for album in db_album:
+                session.execute(
+                    Album.__table__.insert().prefix_with('IGNORE'),
+                    {'id': album.id,
+                     'alb_name': album.alb_name,
+                     'alb_desc': '',
+                     'alb_cover': album.alb_cover,
+                     'alb_size': album.alb_size,
+                     'artist_id': album.artist_id,
+                     'release_time': album.release_time,
+                     'release_comp': album.release_comp}
+                )
+        except exc.OperationalError:
+            print 'OperationalError'
+            session.rollback()
+            continue
+        finally:
+            session.commit()
+            Session.remove()
+            break
 
 if __name__ == "__main__":
     # artist_category_list = get_artist_category_ids()
@@ -398,7 +403,51 @@ if __name__ == "__main__":
         artist_list.append(artist.id)
     Session.remove()
 
-    get_album_by_artist(artist_list)
+    lock = threading.Lock()
+    song_json_queue = Queue.Queue()
+
+    album_thread_count = 10
+    song_thread_count = 50
+
+    artist_count = len(artist_list)
+    song_list_filename = 'song_list_result.txt'
+    album_thread_list = []
+    fp = open(song_list_filename, 'a')
+    for i in range(album_thread_count):
+        begin = artist_count / album_thread_count * i
+        end = artist_count / album_thread_count * (i + 1)
+        artist_list_slice = artist_list[begin:end]
+        t = threading.Thread(target=get_album_by_artist, args=(artist_list_slice,fp,lock))
+        album_thread_list.append(t)
+        t.start()
+
+
+    for t in album_thread_list:
+        t.join()
+
+    fp.close()
+    print 'successfully saved to song_list_result.txt'
+
+    fp = open(song_list_filename,'r')
+    song_list_file = fp.read().split('\n')
+    session = Session()
+    sql_result = session.execute('select id from song').fetchall()
+    song_list_sql = [item[0] for item in sql_result]
+    Session.remove()
+
+    song_list = [ id for id in song_list_file if id not in song_list_sql ]
+    song_count = len(song_list)
+    song_thread_list = []
+    for i in range(song_thread_count):
+        begin = song_count / song_thread_count * i
+        end = song_count / song_thread_count * (i + 1)
+        song_list_slice = song_list[begin:end]
+        t = threading.Thread(target=analyse_song_page, args=(song_list_slice,lock,song_json_queue))
+        song_thread_list.append(t)
+        t.start()
+    for t in song_thread_list:
+        t.join()
+
 
     # lock = threading.Lock()
     # song_queue = Queue.Queue()
